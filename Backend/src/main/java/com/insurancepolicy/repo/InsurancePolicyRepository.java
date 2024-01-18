2ebca10e84608bda921914166ece457eb0cb8133
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-18 13:07:44 +0530

    Delete Backend/src directory

--- Backend/src/main/java/com/insurancepolicy/repo/InsurancePolicyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.insurancepolicy.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.insurancepolicy.entity.InsurancePolicy;
-
-public interface InsurancePolicyRepository extends JpaRepository<InsurancePolicy, Long> {
-
-}
